7ab212fcfc90b6800316d5e554e21a5752d3fc99
tteguayco <teguayco95@gmail.com>
2019-04-16 13:33:37 +0100

    Camera filter finished

--- src/digital-camera-db-crawler/common.py
+++ src/digital-camera-db-crawler/common.py
@@ -5,7 +5,10 @@ HTML_ENCODING = "utf-8"
 def get_html(url):
     f = urllib.request.urlopen(url)
     html = f.read()
-    return html.decode(HTML_ENCODING) 
+    html = html.decode(HTML_ENCODING)
+    html = html.replace("\n", " ")
+    html = html.replace("\t", " ")
+    return html
 
 def add_header_to_url(header, url):
     
