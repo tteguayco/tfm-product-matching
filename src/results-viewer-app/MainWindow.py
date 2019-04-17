d01dd0f5a3cb7dcc462976022857272a8522d031
tteguayco <teguayco95@gmail.com>
2019-04-17 11:53:31 +0100

    Setting event handler for button in GUI

--- src/results-viewer-app/MainWindow.py
+++ src/results-viewer-app/MainWindow.py
@@ -1,4 +1,5 @@
 import PyQt5.QtWidgets as qt
+from functools import partial
 import json
 
 WINDOW_WIDTH = 500
@@ -7,6 +8,20 @@ WINDOW_NAME = "Find match from product offer title"
 
 DEFAULT_PRODUCT_TITLE = "Apple iPhone 4 8GB SIM-Free - Black"
 DEFAULT_JSON_RESULT = "{\"name\": \"Gilbert\", \"wins\": [[\"straight\", \"7♣\"], [\"one pair\", \"10♥\"]]}"
+
+
+def displayJsonResult(productTitleBox, textAreaToDisplayResult):
+    productTitle = productTitleBox.text()
+    result = "{}"
+
+    # get json
+    print("Getting json for '{}'".format(productTitle))
+
+    result = json.loads(result)
+    parsed_result = json.dumps(result, indent=4, sort_keys=False)
+
+    textAreaToDisplayResult.setPlainText(parsed_result)
+
 
 app = qt.QApplication([])
 window = qt.QWidget()
@@ -22,6 +37,11 @@ productOfferTitleTextBox = qt.QLineEdit(DEFAULT_PRODUCT_TITLE)
 findMatchBtn = qt.QPushButton('Find Match')
 jsonResultDisplayTextArea = qt.QPlainTextEdit(parsed_json)
 jsonResultDisplayTextArea.setReadOnly(True)
+
+# Set click event hanlder function for button
+findMatchBtn.clicked.connect(partial(displayJsonResult, 
+                                     productOfferTitleTextBox,
+                                     jsonResultDisplayTextArea))
 
 # Top panel
 topLayout = qt.QHBoxLayout()
