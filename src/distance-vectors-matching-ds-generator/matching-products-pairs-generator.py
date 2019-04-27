33862f770f3c1c761746e38cd15b816a8a01bbf2
tteguayco <teguayco95@gmail.com>
2019-04-27 14:15:26 +0100

    Generated matching and non-matching products pairs dataset

--- src/distance-vectors-matching-ds-generator/matching-products-pairs-generator.py
+++ src/distance-vectors-matching-ds-generator/matching-products-pairs-generator.py
@@ -10,7 +10,8 @@ This script generates an output CSV file with the following fields:
 '''
 
 import pandas as pd
-from random import randint
+import numpy as np
+import random
 
 INPUT_FILEPATH = "../_data/Smartphones.csv"
 OUTPUT_FILEPATH = "../_data/MATCHING_PRODUCTS_PAIRS.csv"
@@ -23,6 +24,8 @@ KNOWN_SMARTPHONES_BRANDS = ["Acer","Apple","Asus","BlackBerry","Bosch","BQ",
 
 OUTPUT_COLS_NAMES = ["ProductTitle1", "ProductTitle2", "ProductPrice1",
     "ProductPrice2", "ProductCurrency1", "ProductCurrency2", "Match"] 
+
+CSV_OUTPUT_FILE_SEP = "\t"
 
 # Data load
 fields = ["Brand", "Title", "Price", "Currency", "MatchingID"]
@@ -44,28 +47,77 @@ df = df.drop_duplicates(subset="Title")
 # products which are not smartphones)
 df = df[df["Brand"].isin(KNOWN_SMARTPHONES_BRANDS)]
 
+print("\nDataframe loaded.")
 print("Number of rows: {}".format(df.shape[0]))
 print("Number of cols: {}".format(df.shape[1]))
 
 df_join_by_matching_id = df.merge(df, on="MatchingID", how="inner")
 
-print(df_join_by_matching_id.head())
-
 # Generate matching pairs
 df_match = df_join_by_matching_id
 
+print("\nMatching products pairs generated.")
+print(df_match.head())
+print("Number of rows: {}".format(df_match.shape[0]))
+print("Number of cols: {}".format(df_match.shape[1]))
+
 # Generate non-matching pairs
-matchingIDs = list(set(df_join_by_matching_id["MatchingID"].tolist()))
-df_no_match = df.Dataframe()
+matchingIDs = list(set(df["MatchingID"].tolist()))
+no_matching_pairs = []
 
-for i in range(len(10000)):
-    random_idx_1 = randint(0, len(matchingIDs) - 1)
-    random_idx_2 = randint(0, len(matchingIDs) - 1)
+for i in range(100000):
+    random_matching_id_idx_1 = random.randint(0, len(matchingIDs) - 1)
+    random_matching_id_idx_2 = random.randint(0, len(matchingIDs) - 1)
 
-    if random_idx_1 != random_idx_2:
-        random_matching_id_1 = matchingIDs[random_idx_1]
-        random_matching_id_2 = matchingIDs[random_idx_2]
-
+    if random_matching_id_idx_1 != random_matching_id_idx_2:
+        random_matching_id_1 = matchingIDs[random_matching_id_idx_1]
+        random_matching_id_2 = matchingIDs[random_matching_id_idx_2]
         
+        # Get lists of indexes of rows that contain chosen MatchingIDs
+        matching_id_row_indexes_1 = np.where(df["MatchingID"] == random_matching_id_1)[0]
+        matching_id_row_indexes_2 = np.where(df["MatchingID"] == random_matching_id_2)[0]
+
+        # Find two product with the the chosen MatchingIDs
+        first_product = df.iloc[random.choice(matching_id_row_indexes_1)].to_dict()
+        second_product = df.iloc[random.choice(matching_id_row_indexes_2)].to_dict()
+       
+        # Add non-matching pair to dict
+        no_matching_pairs.append((first_product, second_product))
+
+print("\nNon-matching products pairs generated.")
+print("Number of pairs: {}".format(len(no_matching_pairs)))
 
 # Export training dataset to CSV file
+with open(OUTPUT_FILEPATH, "w+", encoding="utf-8") as f:
+    
+    # Write column names
+    f.write(CSV_OUTPUT_FILE_SEP.join(OUTPUT_COLS_NAMES) + "\n")
+
+    # Write matching products pairs
+    for i, row in df_match.iterrows():
+        f.write(
+            row["Title_x"] + CSV_OUTPUT_FILE_SEP
+            + row["Title_y"] + CSV_OUTPUT_FILE_SEP
+            + str(row["Price_x"]) + CSV_OUTPUT_FILE_SEP
+            + str(row["Price_y"]) + CSV_OUTPUT_FILE_SEP
+            + row["Currency_x"] + CSV_OUTPUT_FILE_SEP
+            + row["Currency_y"] + CSV_OUTPUT_FILE_SEP
+            + "MATCH\n"
+        )
+
+    # Write non-matching products pairs
+    for pair in no_matching_pairs:
+        p1 = pair[0]
+        p2 = pair[1]
+
+        f.write(
+            p1["Title"] + CSV_OUTPUT_FILE_SEP
+            + p2["Title"] + CSV_OUTPUT_FILE_SEP
+            + str(p1["Price"]) + CSV_OUTPUT_FILE_SEP
+            + str(p2["Price"]) + CSV_OUTPUT_FILE_SEP
+            + p1["Currency"] + CSV_OUTPUT_FILE_SEP
+            + p2["Currency"] + CSV_OUTPUT_FILE_SEP
+            + "UNMATCH\n"
+        )
+
+print("Dataset exported to {}.".format(OUTPUT_FILEPATH))
